3e5656442b4495d2ebef0edc510d5029c4ab7fd6
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-24 11:56:31 -0700

    added files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { addProduct, increment } from './store/counterSlice';
 
 function App() {
-  return (
-    <div className="App">
+  const value = useSelector((state) => state.counter.value);
+  const products = useSelector((state) => state.counter.products);
+  const dipatch = useDispatch();
+  
+  console.log(value);
 
-    </div>
+  const onIncrement = () => {
+    dipatch(increment())
+  }
+  const onAddProduct = () => {
+    dipatch(addProduct("Iphone"));
+  }
+
+  return (
+   <div>
+     <h1>{value}</h1>
+     <button onClick={onIncrement}>increment</button>
+     <button onClick={onAddProduct}>add product</button>
+    
+   </div>
   );
 }
 
---
+++ src/store/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: 0,
+    products: []
+}
+
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+          state.value += 1;
+        },
+
+       addProduct: (state, {payload})=> {
+        state.products.push(payload)
+       }
+    }
+})
+
+export const {increment, addProduct} = counterSlice.actions
+
+export default counterSlice.reducer
