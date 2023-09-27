1542668dab8045824d449a2ae74937322420210e
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-27 11:06:14 -0700

    removed files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 
-import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { addProduct, increment } from './store/counterSlice';
-import Navbar from './components/navbar';
+
 
 
 function App() {
 
-  const value = useSelector((state) => state.counter.value);
-  const products = useSelector((state) => state.counter.products);
-
-  const dispatch = useDispatch();
-console.log(value)
-// console.log(products)
-
-
- const incremenByOne = () => {
-    dispatch(increment() );
- };
-
- const onAddProduct = () => {
-  dispatch(addProduct() );
- };
   return (
    <div>
-    <Navbar />
-    <h1>{value}</h1>
-    <h2>{products}</h2>
-    <button onClick={incremenByOne}>increment</button>
-    <button onClick={onAddProduct}>add products</button>
+    this is my first project
    </div>
   );
   }
