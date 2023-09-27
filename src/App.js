b2e631f2f6266a22d87ab35990d7c83168b5434e
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-27 12:05:36 -0700

    updated files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
-import './App.css';
-
-
-
+import Cart from './components/cart/Cart'
+import Products from './components/cart/Products'
+import Header from './components/cart/Header'
 function App() {
-
   return (
-   <div>
-    this is my first project
-   </div>
-  );
-  }
-
-export default App;
- 
+    <div className="container py-3">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  )
+}
+export default App
